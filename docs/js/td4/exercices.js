5cfaa4f7cb564466d3b2bbda599ae999d84db45e
Enzo Lefevre <elefevre@info.local>
2024-09-30 11:56:59 +0200

    exercices js en cours

--- docs/js/td4/exercices.js
+++ docs/js/td4/exercices.js
@@ -13,3 +13,27 @@ const btreset = document.getElementById('btreset');
 btreset.addEventListener("click", ()=>{
     document.body.style.backgroundColor='';
 });
+
+// ajout d'un élément dans une liste avec un bouton
+const btajout = document.getElementById('btajout');
+btajout.addEventListener("click",()=>{
+    const texte = document.getElementById('text').value;
+    if(texte !== ""){
+        const li= document.createElement("li");
+        li.innerText = texte;
+        list.appendChild(li);
+    };
+});
+
+//addition
+const btplus = document.getElementById('btplus');
+btplus.addEventListener("click",()=>{
+    const nb1 = document.getElementById('nb1').value;
+    const nb2 = document.getElementById('nb2').value;
+    const resultats = document.getElementById('resultats')
+    if(nb1 !=="" && nb2 !==""){
+        resultats.value = parseInt(nb1) + parseInt(nb2);
+    };
+});
+
+// liste deroulantes avec image
