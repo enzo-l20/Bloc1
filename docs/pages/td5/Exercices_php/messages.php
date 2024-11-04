3a79e167efafe53d1ff161b034cfd122cbd28e53
Enzo Lefevre <elefevre@info.local>
2024-11-04 09:26:43 +0100

    modification exercice message.php

--- docs/pages/td5/Exercices_php/messages.php
+++ docs/pages/td5/Exercices_php/messages.php
@@ -5,12 +5,15 @@ echo "<a href='/messages.php?message=Salut'>afficher Salut</a> <br>";
 echo "<a href='/messages.php?message=Aurevoir'>afficher Aurevoir</a> <br>";
 echo "<a href='/messages.php'>afficher le message par défaut</a> <br>";
 
-if (!isset($_GET['message']) or $_GET['message'] == ""){
-    echo "Message par défaut";
-}else{
-    $message = $_GET['message'];
-    echo "$message";
-}
+//if (!isset($_GET['message']) or $_GET['message'] == ""){
+  //  echo "Message par défaut";
+//}else{
+  //  $message = $_GET['message'];
+    //echo "$message";
+//}
 
-
+//on peut aussi utiliser cette methode 
+$default = "message par defaut";
+$message = $_GET['message']??$default;
+echo "$message"
 ?>
