aa1345776f3d087f6ec0ba6cac82d1418b03995c
Enzo Lefevre <enzo32@orange.fr>
2024-12-02 08:46:56 +0100

    modification exercices + index

--- docs/pages/td5/Exercices_php/messages.php
+++ docs/pages/td5/Exercices_php/messages.php
@@ -1,9 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Messages</title>
+</head>
+<body>
+  <a href='messages.php?message=Bonjour'>afficher Bonjour</a> <br>
+  <a href='messages.php?message=Salut'>afficher Salut</a> <br>
+  <a href='messages.php?message=Aurevoir'>afficher Aurevoir</a> <br>
+  <a href='messages.php'>afficher le message par défaut</a> <br>
+</body>
+</html>
 <?php
 
-echo "<a href='/messages.php?message=Bonjour'>afficher Bonjour</a> <br>";
-echo "<a href='/messages.php?message=Salut'>afficher Salut</a> <br>";
-echo "<a href='/messages.php?message=Aurevoir'>afficher Aurevoir</a> <br>";
-echo "<a href='/messages.php'>afficher le message par défaut</a> <br>";
+//echo "<a href='/messages.php?message=Bonjour'>afficher Bonjour</a> <br>";
+//echo "<a href='/messages.php?message=Salut'>afficher Salut</a> <br>";
+//echo "<a href='/messages.php?message=Aurevoir'>afficher Aurevoir</a> <br>";
+//echo "<a href='/messages.php'>afficher le message par défaut</a> <br>";
 
 //if (!isset($_GET['message']) or $_GET['message'] == ""){
   //  echo "Message par défaut";
